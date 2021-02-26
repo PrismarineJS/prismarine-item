f8826ed7233915b7e1d55a9454109668a0da6eb8
u9g <43508353+u9g@users.noreply.github.com>
2021-02-26 14:14:23 -0500

    class-ify so that we can use static class funcs (#14)
    * start the pr
    
    * make item a class & cache it
    
    * make equal static
    
    * removed cache
    
    * bump node ver
    
    Co-authored-by: U9G <winworkswow@gmail.com>

--- index.js
+++ index.js
@@ -1,82 +1,84 @@
 module.exports = loader
 
-function loader (mcVersion) {
-  const mcData = require('minecraft-data')(mcVersion)
+function loader (version) {
+  const mcData = require('minecraft-data')(version)
+  class Item {
+    constructor (type, count, metadata, nbt) {
+      if (type == null) return
 
-  function Item (type, count, metadata, nbt) {
-    if (type == null) return
+      if (metadata instanceof Object && metadata !== null) {
+        nbt = metadata
+        metadata = 0
+      }
 
-    if (metadata instanceof Object && metadata !== null) {
-      nbt = metadata
-      metadata = 0
-    }
+      this.type = type
+      this.count = count
+      this.metadata = metadata == null ? 0 : metadata
+      this.nbt = nbt || null
 
-    this.type = type
-    this.count = count
-    this.metadata = metadata == null ? 0 : metadata
-    this.nbt = nbt || null
-
-    const itemEnum = mcData.findItemOrBlockById(type)
-    if (itemEnum) {
-      this.name = itemEnum.name
-      this.displayName = itemEnum.displayName
-      if ('variations' in itemEnum) {
-        for (const i in itemEnum.variations) {
-          if (itemEnum.variations[i].metadata === metadata) { this.displayName = itemEnum.variations[i].displayName }
+      const itemEnum = mcData.findItemOrBlockById(type)
+      if (itemEnum) {
+        this.name = itemEnum.name
+        this.displayName = itemEnum.displayName
+        if ('variations' in itemEnum) {
+          for (const i in itemEnum.variations) {
+            if (itemEnum.variations[i].metadata === metadata) { this.displayName = itemEnum.variations[i].displayName }
+          }
         }
+        this.stackSize = itemEnum.stackSize
+      } else {
+        this.name = 'unknown'
+        this.displayName = 'unknown'
+        this.stackSize = 1
       }
-      this.stackSize = itemEnum.stackSize
-    } else {
-      this.name = 'unknown'
-      this.displayName = 'unknown'
-      this.stackSize = 1
+    }
+
+    static equal (item1, item2) {
+      if (item1 == null && item2 == null) {
+        return true
+      } else if (item1 == null) {
+        return false
+      } else if (item2 == null) {
+        return false
+      } else {
+        return item1.type === item2.type &&
+            item1.count === item2.count &&
+            item1.metadata === item2.metadata
+      }
+    }
+
+    static toNotch (item) {
+      if (mcData.isNewerOrEqualTo('1.13')) {
+        if (item == null) return { present: false }
+        const notchItem = {
+          present: true,
+          itemId: item.type,
+          itemCount: item.count
+        }
+        if (item.nbt && item.nbt.length !== 0) { notchItem.nbtData = item.nbt }
+        return notchItem
+      } else {
+        if (item == null) return { blockId: -1 }
+        const notchItem = {
+          blockId: item.type,
+          itemCount: item.count,
+          itemDamage: item.metadata
+        }
+        if (item.nbt && item.nbt.length !== 0) { notchItem.nbtData = item.nbt }
+        return notchItem
+      }
+    }
+
+    static fromNotch (item) {
+      if (mcData.isNewerOrEqualTo('1.13')) {
+        if (item.present === false) return null
+        return new Item(item.itemId, item.itemCount, item.nbtData)
+      } else {
+        if (item.blockId === -1) return null
+        return new Item(item.blockId, item.itemCount, item.itemDamage, item.nbtData)
+      }
     }
   }
 
-  Item.equal = (item1, item2) => {
-    if (item1 == null && item2 == null) {
-      return true
-    } else if (item1 == null) {
-      return false
-    } else if (item2 == null) {
-      return false
-    } else {
-      return item1.type === item2.type &&
-      item1.count === item2.count &&
-      item1.metadata === item2.metadata
-    }
-  }
-
-  Item.toNotch = (item) => {
-    if (mcData.isNewerOrEqualTo('1.13')) {
-      if (item == null) return { present: false }
-      const notchItem = {
-        present: true,
-        itemId: item.type,
-        itemCount: item.count
-      }
-      if (item.nbt && item.nbt.length !== 0) { notchItem.nbtData = item.nbt }
-      return notchItem
-    } else {
-      if (item == null) return { blockId: -1 }
-      const notchItem = {
-        blockId: item.type,
-        itemCount: item.count,
-        itemDamage: item.metadata
-      }
-      if (item.nbt && item.nbt.length !== 0) { notchItem.nbtData = item.nbt }
-      return notchItem
-    }
-  }
-
-  Item.fromNotch = (item) => {
-    if (mcData.isNewerOrEqualTo('1.13')) {
-      if (item.present === false) return null
-      return new Item(item.itemId, item.itemCount, item.nbtData)
-    } else {
-      if (item.blockId === -1) return null
-      return new Item(item.blockId, item.itemCount, item.itemDamage, item.nbtData)
-    }
-  }
   return Item
 }
