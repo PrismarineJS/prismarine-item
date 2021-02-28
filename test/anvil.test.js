e86c446375728575312f7ca835d65d190be6f0e8
U9G <winworkswow@gmail.com>
2021-02-28 12:13:44 -0500

    add final item to another test

--- test/anvil.test.js
+++ test/anvil.test.js
@@ -16,7 +16,7 @@ describe('1.8.9 anvil', () => {
     expect(res.item).toStrictEqual(finalItem)
     expect(inverse.item).toStrictEqual(inverseFinalItem)
   })
-  test.only('combine two books', () => {
+  test('combine two books', () => {
     const book1 = Item.fromNotch({ blockId: 403, itemCount: 1, itemDamage: 0, nbtData: { type: 'compound', name: '', value: { StoredEnchantments: { type: 'list', value: { type: 'compound', value: [{ lvl: { type: 'short', value: 5 }, id: { type: 'short', value: 48 } }] } } } } })
     const book2 = Item.fromNotch({ blockId: 403, itemCount: 1, itemDamage: 0, nbtData: { type: 'compound', name: '', value: { StoredEnchantments: { type: 'list', value: { type: 'compound', value: [{ lvl: { type: 'short', value: 1 }, id: { type: 'short', value: 6 } }] } } } } })
     const finalItem = Item.fromNotch({ blockId: 403, itemCount: 1, itemDamage: 0, nbtData: { type: 'compound', name: '', value: { RepairCost: { type: 'int', value: 1 }, StoredEnchantments: { type: 'list', value: { type: 'compound', value: [{ lvl: { type: 'short', value: 5 }, id: { type: 'short', value: 48 } }, { lvl: { type: 'short', value: 1 }, id: { type: 'short', value: 6 } }] } } } } })
@@ -34,8 +34,12 @@ describe('1.8.9 anvil', () => {
     const book = Item.fromNotch({ blockId: 403, itemCount: 1, itemDamage: 0, nbtData: { type: 'compound', name: '', value: { RepairCost: { type: 'int', value: 1 }, StoredEnchantments: { type: 'list', value: { type: 'compound', value: [{ lvl: { type: 'short', value: 5 }, id: { type: 'short', value: 48 } }, { lvl: { type: 'short', value: 5 }, id: { type: 'short', value: 16 } }] } } } } })
     const res = Item.anvil(sword, book, false, undefined)
     const inverse = Item.anvil(book, sword, false, undefined)
+    const finalItem = Item.fromNotch({ blockId: 276, itemCount: 1, itemDamage: 3, nbtData: { type: 'compound', name: '', value: { ench: { type: 'list', value: { type: 'compound', value: [{ lvl: { type: 'short', value: 5 }, id: { type: 'short', value: 16 } }] } }, RepairCost: { type: 'int', value: 3 } } } })
+    const inverseFinalItem = null
     expect(res.xpCost).toStrictEqual(6)
     expect(inverse.xpCost).toStrictEqual(0)
+    expect(res.item).toStrictEqual(finalItem)
+    expect(inverse.item).toStrictEqual(inverseFinalItem)
   })
 
   test('combine book (with incompatible enchants) using creative', () => {
