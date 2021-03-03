2629acd3fec2770edad703d5283ae7b442022bf3
U9G <winworkswow@gmail.com>
2021-03-03 01:05:12 -0500

    finish first wiki test

--- test/anvil.test.js
+++ test/anvil.test.js
@@ -188,12 +188,20 @@ describe('1.16.5 anvil', () => {
       itemOne.enchants = [{ name: 'sharpness', lvl: 3 }, { name: 'knockback', lvl: 2 }, { name: 'looting', lvl: 3 }]
       const itemTwo = new Item(598, 1)
       itemTwo.enchants = [{ name: 'sharpness', lvl: 3 }, { name: 'looting', lvl: 3 }]
+      // expected way
       const expectedItem = new Item(598, 1)
       expectedItem.enchants = [{ name: 'sharpness', lvl: 4 }, { name: 'knockback', lvl: 2 }, { name: 'looting', lvl: 3 }]
       expectedItem.repairCost = 1
       const anvilResults = Item.anvil(itemOne, itemTwo, false, undefined)
       expect(anvilResults.item).toStrictEqual(expectedItem)
       expect(anvilResults.xpCost).toStrictEqual(16)
+      // inverse
+      const inverseAnvilResults = Item.anvil(itemTwo, itemOne, false, undefined)
+      const inverseExpectedItem = new Item(598, 1)
+      inverseExpectedItem.enchants = [{ name: 'sharpness', lvl: 4 }, { name: 'looting', lvl: 3 }, { name: 'knockback', lvl: 2 }]
+      inverseExpectedItem.repairCost = 1
+      expect(inverseAnvilResults.item).toStrictEqual(inverseExpectedItem)
+      expect(inverseAnvilResults.xpCost).toStrictEqual(20)
     })
   })
 })
