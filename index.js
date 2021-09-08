b12fc2a16bcbe7e9537696545eb6f1d25435d178
u9g <43508353+u9g@users.noreply.github.com>
2021-09-07 23:03:55 -0400

    add get/set customLore

--- index.js
+++ index.js
@@ -92,6 +92,17 @@ function loader (version) {
       if (!this.nbt.value.display) this.nbt.value.display = { type: 'compound', value: {} }
       this.nbt.value.display.value.Name = { type: 'string', value: newName }
     }
+    
+    get customLore () {
+      if (Object.keys(this).length === 0) return null
+      return this?.nbt?.value?.display?.value?.Name?.value ?? null
+    }
+
+    set customLore (newLore) {
+      if (!this.nbt) this.nbt = { name: '', type: 'compound', value: {} }
+      if (!this.nbt.value.display) this.nbt.value.display = { type: 'compound', value: {} }
+      this.nbt.value.display.value.Lore = { type: 'string', value: newLore }
+    }
 
     // gets the cost based on previous anvil uses
     get repairCost () {
