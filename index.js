cc2b80b0cca6e2f3389891800d41de1c4af5647f
Frej Alexander Nielsen <frejnielsen59@gmail.com>
2023-07-29 22:54:20 +0200

    update index js

--- index.js
+++ index.js
@@ -307,7 +307,7 @@ function loader (registryOrVersion) {
     set durabilityUsed (value) {
       const where = registry.supportFeature('whereDurabilityIsSerialized')
       if (where === 'Damage') {
-        if (!this?.nbt) return
+        if (!this?.nbt?.value?.Damage) return
         this.nbt.value.Damage = nbt.int(value)
       } else if (where === 'metadata') {
         this.metadata = value
